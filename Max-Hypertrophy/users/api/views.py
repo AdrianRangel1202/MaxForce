69f4e4b553416c4b034b8d5d0deecb4c91748c92
Adrian <adrianrangel1202@gmail.com>
2024-05-27 12:43:27 -0500

    cambio de eliminacion de usuarios completa a logica

--- Max-Hypertrophy/users/api/views.py
+++ Max-Hypertrophy/users/api/views.py
@@ -1,5 +1,4 @@
 from .serializers import UserSerializer
-from .models import User
 from rest_framework.response import Response
 from rest_framework.views import  status
 from rest_framework.decorators import api_view
