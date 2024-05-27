ec7cb10922a3045493130be3988fbd6867919a8e
Adrian <adrianrangel1202@gmail.com>
2024-05-26 20:26:58 -0500

    add

--- Max-Hypertrophy/users/api/serializers.py
+++ Max-Hypertrophy/users/api/serializers.py
@@ -9,5 +9,14 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ("username","email","name","last_name", "password")
 
 
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
     
-
+    def update(self, instance, validated_data):
+        updated_user = super().update(instance, validated_data)
+        updated_user.set_password(validated_data['password'])
+        updated_user.save()
+        return updated_user
