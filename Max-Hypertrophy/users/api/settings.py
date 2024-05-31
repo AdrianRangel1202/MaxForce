410828c5c5ddb9d31e3ad3469e54f1e43ec3af14
Adrian <adrianrangel1202@gmail.com>
2024-05-31 14:45:22 -0500

    config de data base in setting.py

--- Max-Hypertrophy/users/api/settings.py
+++ Max-Hypertrophy/users/api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from .db_config import POSTGRESQL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,16 +76,10 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'UsersDB',
-        'USER': 'postgres',
-        'PASSWORD': '123456789',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
+
+
+DATABASES = POSTGRESQL
+
 
 
 # Password validation
